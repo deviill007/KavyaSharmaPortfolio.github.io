711ddafa8e341cea525b3c46ff1978294e7ca9a6
Kavya Sharma <8kavyasharma@gmail.com>
2023-06-27 17:41:19 +0530

    New updated design, content updates.

--- Script.js
+++ Script.js
@@ -1,16 +1,5 @@
-//form submit
-// Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "username",
-//     Password: "password",
-//     To: 'them@website.com',
-//     From: "you@isp.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body"
-// }).then(
-//     message => alert(message)
-// );
 
+document.addEventListener('DOMContentLoaded', function() {
 //menu icon toggle
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.Navbar');
@@ -53,10 +42,69 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
-//Popup Message
-let popup = document.getElementById("popup")
+// Read Me button 
+    var readMoreBtn = document.getElementById('RMbutton');
+    var extraParagraphs = document.getElementsByClassName('extra-paragraph');
+    var readMoreSection = document.getElementById('about');
+  
+    readMoreBtn.addEventListener('click', function() {
+      // Toggle visibility of extra paragraphs
+      for (var i = 0; i < extraParagraphs.length; i++) {
+        extraParagraphs[i].classList.toggle('show');
+      }
+  
+      // Update button text
+      if (readMoreBtn.textContent === 'Read More') {
+        readMoreBtn.textContent = 'Read Less';
+      } else {
+        readMoreBtn.textContent = 'Read More';
+        scrollToReadMore();
+      }
+    });
+    function scrollToReadMore() {
+        var readMoreBtnOffsetTop = readMoreBtn.getBoundingClientRect().top;
+        var scrollPosition = window.pageYOffset + readMoreBtnOffsetTop - window.innerHeight + 100;
+        window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+    }
+  });
+  
 
-function openPopup(){
-    console.log(popup)
-    popup.classList.add("open-popup");
+  //form filled toggle
+  function toggle(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    var blur = document.getElementById("contact");
+    blur.classList.add("activeblur");
+    document.body.classList.add("no-scroll");
+    var appear = document.getElementById('popup');
+    appear.classList.add("appear");
+}
+
+// Get a reference to the Home button element
+var homeButton = document.getElementById("home-button");
+
+// Add an event listener to the Home button
+homeButton.addEventListener("click", function() {
+  // Reload the page
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  location.reload();
+});
+
+//Contact form
+function sendmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "8kavyasharma@gmail.com",
+        Password : "F950F5331200575F29C9E05E875A6BD8121E",
+        To : '8kavyasharma@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Message : "Name: " + document.getElementById("name").value + "<br> Mobile Number: " + document.getElementById("mobile") + "<br> Message: " + document.getElementById("message")
+    }).then(
+        function (message) {
+        toggle(event); // Call the toggle function
+      },
+      function (error) {
+        alert("Failed to send email. Error: " + error);
+      }
+    );
 }
